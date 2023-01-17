b656694427c0a36dd3172174e034a2ef55cdee4b
Rich House <rich@richhouse.co.uk>
2023-01-17 21:45:05 +0000

    adds more data

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useEffect, useState } from 'react';
-import { LineChart } from 'react-native-gifted-charts';
+import { BarChart, PieChart } from 'react-native-gifted-charts';
 import getData from './components/getData';
 
 
 export default function App() {
-  const [data, setData] = useState([]);
+  const [forecastData, setForecastData] = useState([]);
+  const [generationData, setGenerationData] = useState([])
+  const [currentData, setCurrentData] = useState({});
+
   
   useEffect(() => {
-    getData(setData);
+    getData(setForecastData, setCurrentData, setGenerationData);
   }, [])
   
 
   return (
     <View style={styles.container}>
-      <LineChart data = {data} />
-      <Text>Start</Text>
+      <Text>Current Carbon Intensity is {currentData.index}</Text>
+      <Text>24 hr Forecast:</Text>
+      <BarChart data={forecastData} />
+      <PieChart donut showText focusOnPress textColor="slategrey" data={generationData} textSize={15}/>
       <StatusBar style="auto" />
     </View>
   );
--- components/getData.js
+++ components/getData.js
@@ -1,15 +1,70 @@
+import { Text } from 'react-native';
 import axios from 'axios';
 
 
-export default async function getData(setter) {
+export default async function getData(setForecast, setCurrent, setGeneration) {
   const date = new Date(Date.now());
   try {
-    const rawData = await axios.get(`https://api.carbonintensity.org.uk/intensity/${date.toISOString()}/fw24h`);
-    
-    const intensityForecastValues = rawData.data.data.map((obj) => ({ value: obj.intensity.forecast, label: new Date(obj.from).toTimeString().slice(0, 5)}))
-    console.log(intensityForecastValues);
-    setter(intensityForecastValues);
-  } catch(error) {
+    const forecastRaw = await axios.get(`https://api.carbonintensity.org.uk/intensity/${date.toISOString()}/fw24h`);
+    const currentData = await axios.get('https://api.carbonintensity.org.uk/intensity');
+    const generationData = await axios.get('https://api.carbonintensity.org.uk/generation');
+
+    const intensityForecastValues = forecastRaw.data.data.filter((obj, index) => 
+      index % 2 
+      ).map((obj) => {
+        const value = obj.intensity.forecast;
+        const label = new Date(obj.from).toTimeString().slice(0, 5);
+        const frontColor = getColour(value);
+          const topLabelComponent = () => (
+            <Text style={{color: frontColor, fontSize: 18, marginBottom: 6}}>{value}</Text>
+          )
+        return {
+        value,
+        label,
+        frontColor,
+        topLabelComponent
+      }});
+
+    const pieValues = generationData.data.data.generationmix.map(({fuel, perc}) => ({
+      value: perc,
+      text: fuel,
+      color: generationColours[fuel]
+    }));
+
+    console.log(pieValues)
+
+    setForecast(intensityForecastValues);
+    setCurrent(currentData.data.data[0].intensity);
+    setGeneration(pieValues);
+  } catch (error) {
     console.error(error)
   }
+}
+
+const getColour = (value) => {
+  return value > 200
+  ? 'darkred'
+  : value > 180 
+  ? 'red'
+  : value > 150
+  ? 'darkorange' 
+  : value > 120
+  ? 'orange'
+  : value > 100
+  ? 'gold'
+  : value > 80
+  ? 'forestgreen'
+  : 'lightgreen';
+}
+
+const generationColours = {
+  wind: 'cyan',
+  biomass: 'darkolivegreen',
+  coal: 'darkgrey',
+  nuclear: 'goldenrod',
+  imports: 'pink',
+  solar: 'gold',
+  hydro: 'aqua',
+  other: 'grey',
+  gas: 'aquamarine'
 }
